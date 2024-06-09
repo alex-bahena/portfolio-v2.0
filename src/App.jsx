bfde37e63774915cf55dd7d256265fe486a7904e
Alex Bahena <alexbahena@Alexs-MacBook-Pro.local>
2024-06-09 19:01:22 -0400

    Navbar component first push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+
+  return (
+<div className="container">
+
+<Navbar></Navbar>
+</div>
+
+  )
+}
+
+export default App
