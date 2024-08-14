91633b0a1063e1d736638c90016297915bfbe5de
Alex Bahena <alejandro.bahsan@gmail.com>
2024-08-14 13:55:19 -0400

    Navbar - Mobile view update.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,12 +8,13 @@ function Navbar() {
     const isMobile = windowWidth <= 768;
 
     const [isMenuOpen, setIsMenuOpen] = useState(true);
-    
+
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
     useEffect(() => {
+
         /* Render the last state of the dark mode theme stored in localStorage */
         const darkTheme = localStorage.getItem('DARK_MODE');
         document.body.classList.add(darkTheme);
@@ -21,7 +22,7 @@ function Navbar() {
             const clickedElement = event.target.closest('a');
             if (clickedElement) {
                 event.preventDefault();
-             
+                setIsMenuOpen(isMenuOpen);
                 const activeElements = document.querySelectorAll('a.active');
                 activeElements.forEach(element => element.classList.remove('active'));
 
@@ -38,7 +39,7 @@ function Navbar() {
         return () => {
             document.removeEventListener('click', handleClick);
         };
-    }, []);
+    }, [isMenuOpen]);
 
     const handleAnchorClick = (event, sectionId) => {
         event.preventDefault();
@@ -46,8 +47,8 @@ function Navbar() {
         const navbarOffset = 50;
         const elementPosition = element.getBoundingClientRect().top;
         const offsetPosition = elementPosition + window.scrollY - navbarOffset
-        
-    
+
+
         window.scrollTo({
             top: offsetPosition,
             behavior: 'smooth'
@@ -56,59 +57,52 @@ function Navbar() {
 
 
     return (
-        <nav className='navbar'>
+        <nav className={!isMenuOpen ? "navbar nav-active" : 'navbar'}>
             <div className="logo">
-               <h3>Alex Bahena</h3>
-            </div>  
+                <h3>Alex Bahena</h3>
+            </div>
             {isMobile ? <>
                 <div className='mobile-right-menu'>
-            {!isMenuOpen && <>
-                <DarkMode></DarkMode>
-            </>}
-            <span className="hamburger-menu material-symbols-sharp" onClick={toggleMenu}>
-                {isMenuOpen ? "drag_handle" : "close"}
-            </span>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-1 nav-web' : 'nav-mobile'}>
-                <a onClick={(e) => handleAnchorClick(e, 'about')}>about</a>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-2 nav-web' : 'nav-mobile'}>
-                <a href="#experience" >experience</a>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-3 nav-web' : 'nav-mobile'}>
-                <a  onClick={(e) => handleAnchorClick(e, 'projects')}>projects</a>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-4 nav-web' : 'nav-mobile'}>
-                <a  href="#resume">resume</a>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-5 nav-web' : 'nav-mobile'}>
-                <a href="#contact" onClick={(e) => handleAnchorClick(e, 'contact')}>contact</a>
-            </div>
+                    {!isMenuOpen && <>
+                        <DarkMode></DarkMode>
+                    </>}
+                    <span className="hamburger-menu material-symbols-sharp" onClick={toggleMenu}>
+                        {isMenuOpen ? "drag_handle" : "close"}
+                    </span>
+                </div>
+                <div className={isMenuOpen ? 'nav-web' : 'nav-mobile'}>
+                    <a onClick={(e) => handleAnchorClick(e, 'about')}>about</a>
+                    <a href="#experience" >experience</a>
+                    <a onClick={(e) => handleAnchorClick(e, 'projects')}>projects</a>
+                    <a href="#resume">resume</a>
+                    <a href="#contact" onClick={(e) => handleAnchorClick(e, 'contact')}>contact</a>
+                </div>
+
             </>
-            :    <>
-        <div className='center-menu'>
-            <div className={isMenuOpen ? 'nav-item-1 nav-web' : 'nav-mobile'}>
-                <a onClick={(e) => handleAnchorClick(e, 'about')}>about</a>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-2 nav-web' : 'nav-mobile'}>
-                <a href="#experience" >experience</a>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-3 nav-web' : 'nav-mobile'}>
-                <a  onClick={(e) => handleAnchorClick(e, 'projects')}>projects</a>
-            </div>
-        </div>
-        <div className='right-menu'>
-            <div className={isMenuOpen ? 'nav-item-4 nav-web' : 'nav-mobile'}>
-                <a  href="#resume">resume</a>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-5 nav-web' : 'nav-mobile'}>
-                <a href="#contact" onClick={(e) => handleAnchorClick(e, 'contact')}>contact</a>
-            </div>
-            <div className={isMenuOpen ? 'nav-item-6 nav-web' : 'nav-mobile'}>
-           <DarkMode></DarkMode>
-            </div>
-        </div>
-            </>  
+                : <>
+                    <div className='center-menu'>
+                        <div className='nav-item-1 nav-web' >
+                            <a onClick={(e) => handleAnchorClick(e, 'about')}>about</a>
+                        </div>
+                        <div className='nav-item-2 nav-web' >
+                            <a href="#experience" >experience</a>
+                        </div>
+                        <div className='nav-item-3 nav-web' >
+                            <a onClick={(e) => handleAnchorClick(e, 'projects')}>projects</a>
+                        </div>
+                    </div>
+                    <div className='right-menu'>
+                        <div className='nav-item-4 nav-web' >
+                            <a href="#resume">resume</a>
+                        </div>
+                        <div className='nav-item-5 nav-web' >
+                            <a href="#contact" onClick={(e) => handleAnchorClick(e, 'contact')}>contact</a>
+                        </div>
+                        <div className='nav-item-6 nav-web' >
+                            <DarkMode></DarkMode>
+                        </div>
+                    </div>
+                </>
             }
         </nav>
     )
