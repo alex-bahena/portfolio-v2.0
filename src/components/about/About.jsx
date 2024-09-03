020484af8d1081d49afda9653445b8876b13c712
Alex Bahena <alejandro.bahsan@gmail.com>
2024-09-03 16:06:47 -0400

    About section revised.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,5 @@
 import './About.css'
-import profilephoto from '/assets/profilephoto.jpg'
+import profilephoto from '/assets/profilephoto.png'
 import useWindowSize from '../../hooks/useWindowSize';
 
 
@@ -9,53 +9,36 @@ function About() {
 
     return (
         <section id="about" className="about">
-              <div className='profile-photo'>
-                        <img src={profilephoto} alt="profile-photo" />
-                    </div>
             {isMobile ? (
                 <>
                     <div className='about-me'>
-                        <h2 className='title-decoration'><span>About me</span></h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur
-                            adipisicing elit. Sapiente perspiciatis
-                            tempora ea amet consectetur reiciendis
-                            ipsa vel, voluptatibus quo enim,
-                            minima suscipit sit saepe quia
-                            tempore recusandae dolores
-                            praesentium numquam.
+                        <div className='section-title'>
+                            <h2 className='title-decoration'><span>About me</span></h2>
+                        </div>
+                        <div className='profile-photo'>
+                            <img src={profilephoto} alt="profile-photo" />
+                        </div>
+                        <p className='line-jump'>Hey there! I’m Alex, a Mechatronics Engineer located in the <span className='emphasis-text'>Greater Toronto Area</span>. With a background in automation spanning over five years, I’ve recently transitioned into software development.</p>
 
-                            Lorem ipsum, dolor sit amet consectetur
-                            adipisicing elit. Sapiente perspiciatis
-                            tempora ea amet consectetur reiciendis
-                            ipsa vel, voluptatibus quo enim,
-                            minima suscipit sit saepe quia
-                            tempore recusandae dolores
-                            praesentium numquam.
-                        </p>
+                        <p className='line-jump'>By combining my engineering skills and passion for coding, I<span className='emphasis-text'> specialized</span> in creating innovative and efficient solutions to real-world problems.</p>
+
+                        <p> Outside of work, I love exploring small towns with my family and capturing unique perspectives through my camera and drone. My hobbies keep me inspired and curious about the world around me.</p>
                     </div>
                 </>
             ) : (
                 <>
+                    <div className='profile-photo'>
+                        <img src={profilephoto} alt="profile-photo" />
+                    </div>
                     <div className='about-me'>
                         <h2 className='title-decoration'><span>About me</span></h2>
                     </div>
                     <div className='about-description'>
-                        <p>Lorem ipsum, dolor sit amet consectetur
-                            adipisicing elit. Sapiente perspiciatis
-                            tempora ea amet consectetur reiciendis
-                            ipsa vel, voluptatibus quo enim,
-                            minima suscipit sit saepe quia
-                            tempore recusandae dolores
-                            praesentium numquam.
+                        <p className='line-jump'>Hey there! I’m Alex, a Mechatronics Engineer located in the <span className='emphasis-text'>Greater Toronto Area</span>. With a background in automation spanning over five years, I’ve recently transitioned into software development.</p>
 
-                            Lorem ipsum, dolor sit amet consectetur
-                            adipisicing elit. Sapiente perspiciatis
-                            tempora ea amet consectetur reiciendis
-                            ipsa vel, voluptatibus quo enim,
-                            minima suscipit sit saepe quia
-                            tempore recusandae dolores
-                            praesentium numquam.
-                        </p>
+                        <p className='line-jump'>By combining my engineering skills and passion for coding, I<span className='emphasis-text'> specialized</span> in creating innovative and efficient solutions to real-world problems.</p>
+
+                        <p> Outside of work, I love exploring small towns with my family and capturing unique perspectives through my camera and drone. My hobbies keep me inspired and curious about the world around me.</p>
                     </div>
                 </>)
             }
