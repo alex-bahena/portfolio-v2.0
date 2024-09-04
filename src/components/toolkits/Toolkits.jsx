39c293e8de200dde8b6a801c8fdaf40ca905c8ba
Alex Bahena <alejandro.bahsan@gmail.com>
2024-09-04 13:16:03 -0400

    Toolkit component

---
+++ src/components/toolkits/Toolkits.jsx
@@ -0,0 +1,16 @@
+import './Toolkits.css'
+
+
+function Toolkits() {
+    return (
+
+<section className="Toolkits">
+<div className='projects-title'>
+                <h2 className='title-decoration'><span>Toolkits</span></h2>
+            </div>
+</section>
+    )
+}
+
+
+export default Toolkits
