353a595ed3b702c5e4c785b8253e2a65083a211b
Alex Bahena <alejandro.bahsan@gmail.com>
2024-09-12 00:09:54 -0400

    Contact-component done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,7 +7,9 @@ function Contact() {
         <div id='contact' className='contact-wrapper'>
             <h2 className='title-decoration'><span>Conctact</span></h2>
             <div className='contact-content'>
-            I am currently looking for a position in software development or a related field. If you have any job openings available, I would appreciate it if you could contact me.
+                <p>
+                    I’m currently looking for new tech positions. I would appreciate any job opportunities or referrals. <br />Feel free to contact me with relevant openings. Thank you for your support!
+                </p>
             </div>
             <div className='contact-button'>
                 <button><a href="mailto:alex.bahena.ca@gmail.com">Say Hello!</a></button>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,9 +6,9 @@ function footer() {
 
 <section className="footer">
     <div className='footer-content'>
-                <a href=""><span className="fa fa-github"></span></a>
-                <a href=""><span className="fa fa-linkedin"></span></a>
-                <p className='creator-label'>Alex Bahena - 2024</p>
+                <a rel="noopener noreferrer" target="_blank" href="https://github.com/alex-bahena"><span className="fa fa-github"></span></a>
+                <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/alex-bahena/"><span className="fa fa-linkedin"></span></a>
+                <p className='creator-label'>Developed by Alex Bahena - 2024</p>
     </div>
 </section>
     )
