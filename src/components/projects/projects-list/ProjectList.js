1d755b616129dadc27be6d4e241cdf71cbe81d5d
Alex Bahena <alejandro.bahsan@gmail.com>
2024-09-12 12:48:49 -0400

    projects image-folder and rutes moved to assests

--- src/components/projects/projects-list/ProjectList.js
+++ src/components/projects/projects-list/ProjectList.js
@@ -1,27 +1,27 @@
 const projects = [
     {
-        p1_thumbnail: "/src/components/projects/projects-img/SII-Website.png",
+        p1_thumbnail: "assets/projects-img/SII-Website.png",
         p1_name: "SIIPDC Web",
         p1_description: 'A single-page application created with Angular 17 and Tailwind CSS for a professional technical crew in Mexico.',
         p1_technologies: ['AngularJS', 'TailwindCSS'],
         p1_links: ['https://www.siipdc.com/']
     },
     {
-        p2_thumbnail: "/src/components/projects/projects-img/Dynamic-team.png",
+        p2_thumbnail: "assets/projects-img/Dynamic-team.png",
         p2_name: "Dynamic team profile generator",
         p2_description: "This Node.js command-line application efficiently manages and displays employee information, generating a well-organized HTML webpage.",
         p2_technologies: ["HTML", "JavaScript", "Boostrap", "Inquirer.js", "Jest.js"],
         p2_links: ["https://github.com/alex-bahena/Dynamic-team-profile-generator", "https://drive.google.com/file/d/1vbW5DFR0WPc9xNLEWIrcepT_kDaYzKoZ/view"]
     },
     {
-        p3_thumbnail: "/src/components/projects/projects-img/Dynamic-Readme.png",
+        p3_thumbnail: "assets/projects-img/Dynamic-Readme.png",
         p3_name: "Dynamic README file generator",
         p3_description: "A Node.js command-line tool integrated with Inquirer.js that auto-generates README.md templates to save time for project developers.",
         p3_technologies: ["JavaScript", "Node.js", "Inquirer.js"],
         p3_links: ["https://github.com/alex-bahena/Dynamic-README-Generator/tree/master", "https://drive.google.com/file/d/1KqcdsAU1tB-Me3seFD1IpReFs_S3nkjg/view"]
     },
     {
-        p4_thumbnail: "/src/components/projects/projects-img/Google-books-api.png",
+        p4_thumbnail: "assets/projects-img/Google-books-api.png",
         p4_name: "Google Books API - Search engine",
         p4_description: "A React.js and Node.js app using the Google Books API, allowing users to easily search and view book details with a dynamic interface.",
         p4_technologies: ["React.js", "Node.js", "Express.js", "GraphQL", "MongoDB" ],
